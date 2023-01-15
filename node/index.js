ee06c2f2ea58102321a3c9db781e01e440b3cbc0
Victor Freixinho <victor.freixinho@getstic.ky>
2023-01-15 14:05:34 -0300

    Change to node alpine

--- node/index.js
+++ node/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const config = {
-  host: "db",
+  host: "node-db",
   user: "root",
   password: "root",
   database: "nodedb",
