077585cfdd209992ea06295a98d8c90a195b3365
Victor Freixinho <victor.freixinho@getstic.ky>
2023-01-21 13:55:32 -0300

    Fixes

--- node/index.js
+++ node/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const config = {
-  host: "node-db",
+  host: "nodedb",
   user: "root",
   password: "root",
   database: "nodedb",
